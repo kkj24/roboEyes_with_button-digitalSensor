1fd6a52b9ba6574e48059f6464dac1481d74ac06
kkj24 <kjkhemal@gmail.com>
2026-01-08 23:28:49 +0700

    Add files via upload

---
+++ roboEyes_simple.ino
@@ -0,0 +1,73 @@
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <FluxGarage_RoboEyes.h>
+
+#define LCD_WIDTH 128
+#define LCD_HEIGHT 64
+#define LCD_RST -1
+#define LCD_ADDRESS 0x3C //you can change your address oled
+
+Adafruit_SSD1306 lcd(LCD_WIDTH, LCD_HEIGHT, &Wire, LCD_RST);
+RoboEyes<Adafruit_SSD1306> eyes(lcd);
+
+#define FPS 100 //FPS of eyes animation <Range::60-100|Recommended>
+
+constexpr int in = 13; //button or digital sensor at pin 13
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+
+  if(!lcd.begin(SSD1306_SWITCHCAPVCC, LCD_ADDRESS)) {
+    Serial.println("Alocation failed.. try to change lcd address or check the wiring");
+    return;
+  }
+
+  Serial.println("Animation ready");
+
+  eyes.begin(LCD_WIDTH, LCD_HEIGHT, FPS);
+  eyes.setPosition(DEFAULT);
+  eyes.close();
+
+  pinMode(in, INPUT);
+
+  delay(1000);
+
+}
+
+void loop() {
+  eyes.update();
+  eyes.open();
+
+  bool input = digitalRead(in);
+  static bool current = true;
+  static int expression = 0;
+
+  if(input != current) {
+    expression++;
+    if(expression > 2) expression = 0; //reset expression state
+  }
+
+  switch(expression) {
+    case 0: //default expression = 0
+      eyes.setMood(DEFAULT);
+    break;
+
+    case 1: //happy expression = 1
+      eyes.setMood(HAPPY);
+    break;
+
+    case 2: //tired expression = 2
+      eyes.setMood(TIRED);
+    break;
+
+  /*
+  you also can add some expression or reaction at here with same format..
+  */
+
+  }
+
+  current = input;
+
+}
